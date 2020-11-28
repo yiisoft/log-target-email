66a8153f4c3150ab5fcd453ae72185f6b0c33da1
devanych <mail@devanych.ru>
2020-11-28 13:51:04 +0300

    Remove use LogLevel

--- tests/EmailTargetTest.php
+++ tests/EmailTargetTest.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Yiisoft\Log\Target\Email\Tests;
 
 use PHPUnit\Framework\TestCase;
-use Psr\Log\LogLevel;
 use Yiisoft\Log\LogRuntimeException;
 use Yiisoft\Log\Target\Email\EmailTarget;
 use Yiisoft\Mailer\BaseMailer;
