99c786ad57929e1e1a7f002e89649f0d9cb16311
devanych <mail@devanych.ru>
2021-01-07 13:55:50 +0300

    Replace Container on ContainerInterface in configuration

--- config/common.php
+++ config/common.php
@@ -2,8 +2,8 @@
 
 declare(strict_types=1);
 
+use Psr\Container\ContainerInterface;
 use Psr\Log\LoggerInterface;
-use Yiisoft\Di\Container;
 use Yiisoft\Log\Logger;
 use Yiisoft\Log\Target\Email\EmailTarget;
 use Yiisoft\Mailer\MailerInterface;
@@ -13,7 +13,7 @@ use Yiisoft\Mailer\MailerInterface;
 return [
     LoggerInterface::class => static fn (EmailTarget $emailTarget) => new Logger([$emailTarget]),
 
-    EmailTarget::class => static function (Container $container) use ($params) {
+    EmailTarget::class => static function (ContainerInterface $container) use ($params) {
         $emailTarget = new EmailTarget(
             $container->get(MailerInterface::class),
             $params['yiisoft/log-target-email']['emailTarget']['emailTo'],
